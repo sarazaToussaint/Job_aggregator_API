5a7bf96063739f176b04038d743c36717e4e00e8
SarazaToussaint <tmsaraza@gmail.com>
2025-10-17 05:08:52 +0200

    Add update and delete endpoints for subscribers

--- app/crud/subscribers.py
+++ app/crud/subscribers.py
@@ -12,3 +12,24 @@ def create_subscriber(db: Session, subscriber: schemas.SubscriberCreate):
     db.commit()
     db.refresh(db_subscriber)
     return db_subscriber
+
+
+def update_subscriber(db: Session, subscriber_id: int, subscriber_update: schemas.SubscriberUpdate):
+    subscriber = db.query(models.Subscriber).filter(models.Subscriber.id == subscriber_id).first()
+    if not subscriber:
+        return None
+    update_data = subscriber_update.dict(exclude_unset=True)
+    for key, value in update_data.items():
+        setattr(subscriber, key, value)
+    db.commit()
+    db.refresh(subscriber)
+    return subscriber
+
+
+def delete_subscriber(db: Session, subscriber_id: int):
+    subscriber = db.query(models.Subscriber).filter(models.Subscriber.id == subscriber_id).first()
+    if not subscriber:
+        return False
+    db.delete(subscriber)
+    db.commit()
+    return True
